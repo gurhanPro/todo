9c97a6d67b8ad9f36a36936f3842f5873c1a889d
gurhanpro <ziyaadyare@gmail.com>
2020-12-04 20:44:06 +0200

    added routing

--- Todo.UI/ui/src/App.js
+++ Todo.UI/ui/src/App.js
@@ -1,17 +1,24 @@
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+
 import { store } from "./store";
 
 import './App.css';
 import Auth from './containers/auth';
+import Todos from "./containers/todos";
 
 function App() {
   return (
     <Provider store={store}>
+    <Router history={createHistory}>
      <div className="container">
         <div className="App">
-        <Auth />
-        </div>
+        <Route exact path="/" component={Auth} />
+        <Route exact path="/todos" component={Todos} />
+      </div>
     </div>
+  </Router>
   </Provider>
   );
 }
---
+++ Todo.UI/ui/src/containers/todos/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todos extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Todos here</h1>
+            </div>
+        )
+    }
+}
