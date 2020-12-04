5fc62af5959aff69f0cfe5185f5b1a127bf922a4
gurhanpro <ziyaadyare@gmail.com>
2020-12-04 19:10:51 +0200

    removed sample code, created component container folder structure

---
+++ Todo.UI/ui/src/containers/auth/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Auth extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Auth here</h1>
+            </div>
+        )
+    }
+}
